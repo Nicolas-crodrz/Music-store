da380eeb5465222776c24e983fe2c1cc1d11d7f3
Nicolas <73233224+Nicolas-Cabrera-Rodriguez@users.noreply.github.com>
2023-01-19 18:05:14 +0000

    inicio

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  $('.headerContenido').slick({
+  /*slidesToShow: 3,
+  dots:true,
+  centerMode: true,*/
+
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  });
+});
